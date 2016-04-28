5542b31db67fd047a36f01075457912c00ab8074
Billy Konesavanh <billykonesavanh17@gmail.com>
2016-04-28 09:50:48 -0600

    Added to GraphPanel

--- src/ctec/view/GraphPanel.java
+++ src/ctec/view/GraphPanel.java
@@ -1,6 +1,25 @@
 package ctec.view;
 
-public class GraphPanel
-{
+import javax.swing.JPanel;
+import java.awt.Graphics;
+import java.awt.Rectangle;
 
+public class GraphPanel extends JPanel
+{
+	private int [] graphSource;
+	
+	public GraphPanel()
+	{
+		//Initialize graphSource here
+	}
+	
+	protected void paintComponent(Graphics currentGraphics)
+	{
+		super.paintComponent(currentGraphics);
+		for(int index = 0; index < graphSource.length; index++)
+		{
+			//Draw a rectangle for each value in the array
+			//Height constant, width value array
+		}
+	}
 }
